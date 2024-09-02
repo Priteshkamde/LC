e2060d4039455c37c410d303268034a99240173a
Pritesh Kamde <40750377+Priteshkamde@users.noreply.github.com>
2024-09-02 12:23:17 +0530

    Time: 692 ms (89.58%), Space: 0B (100.00%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,4 +1,15 @@
 /* Write your PL/SQL query statement below */
 
-select P.product_name, S.year, S.price from Sales S
-inner join Product P on P.product_id = S.product_id;
+-- select P.product_name, S.year, S.price from Sales S
+-- inner join Product P on P.product_id = S.product_id;
+
+
+with sales_details as 
+ (  
+    select P.product_name, S.year, S.price 
+    from Sales S
+    inner join Product P 
+    on P.product_id = S.product_id;
+ )
+ 
+ select product_name, year, price from sales_details;
