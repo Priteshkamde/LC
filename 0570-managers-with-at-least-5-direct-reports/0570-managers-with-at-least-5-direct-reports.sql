f18aa67404a0cc1c5c745ab14c335731de1cac6d
Pritesh Kamde <40750377+Priteshkamde@users.noreply.github.com>
2024-09-03 03:52:39 +0530

    Time: 446 ms (67.67%), Space: 0B (100.00%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,15 @@
 /* Write your PL/SQL query statement below */
 
-select e1.name from employee e1 where id in (
-    select managerId
-    from employee
-    group by managerId
-    having count(*) > 4
-);
+-- select e1.name from employee e1 where id in (
+--     select managerId
+--     from employee
+--     group by managerId
+--     having count(*) > 4
+-- );
+
+select m.name
+from employee e 
+inner join employee m 
+    on e.managerId = m.id
+group by e.managerId, m.name
+having count(*) >= 5;
