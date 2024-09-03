f2eabbdc127170aadb8018845d446c02c5bd1ef1
Pritesh Kamde <40750377+Priteshkamde@users.noreply.github.com>
2024-09-03 06:07:11 +0530

    Time: 309 ms (99.72%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,7 +1,23 @@
 /* Write your PL/SQL query statement below */
 
 
-select max(salary) as "SecondHighestSalary" from Employee 
-where salary < (
-select max(salary) from Employee)
-;
+-- select max(salary) as "SecondHighestSalary" from Employee 
+-- where salary < (
+-- select max(salary) from Employee)
+-- ;
+
+
+-- select salary as "SecondHighestSalary" from (
+-- select salary, rank() over (order by salary desc) as sal_rank from Employee
+--     where salary is not null
+--     ) ranked 
+    
+--     where sal_rank = 2;
+    
+    
+WITH CTE AS
+			(SELECT Salary, DENSE_RANK () OVER (ORDER BY Salary desc) AS RANK_desc
+			   FROM Employee)
+SELECT MAX(salary) AS SecondHighestSalary
+  FROM CTE
+ WHERE RANK_desc = 2
