9e183099291a69fbc6d5e0a30c70ed6b31a9815c
Pritesh Kamde <40750377+Priteshkamde@users.noreply.github.com>
2024-09-03 06:20:43 +0530

    Time: 507 ms (97.31%), Space: 0B (100.00%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,5 +1,8 @@
 /* Write your PL/SQL query statement below */
 
-select user_id, substr(upper(name), 1, 1) || substr(lower(name), 2, length(name)) as name
-from Users
-order by user_id;
+select 
+    user_id, substr(upper(name), 1, 1) || substr(lower(name), 2) as name
+from 
+    Users
+order by 
+    user_id;
