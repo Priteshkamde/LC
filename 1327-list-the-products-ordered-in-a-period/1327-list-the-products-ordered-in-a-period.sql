d673c9929717ad6efc9f85aa79743900e1b6f02b
Pritesh Kamde <40750377+Priteshkamde@users.noreply.github.com>
2024-09-16 10:42:52 -0700

    Time: 1714 ms (5.18%), Space: 0B (100.00%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -1,11 +1,20 @@
 /* Write your PL/SQL query statement below */
 
 
-select p.product_name, sum(unit) as unit 
-from Products p 
-join Orders o 
+-- select p.product_name, sum(unit) as unit 
+-- from Products p 
+-- join Orders o 
+-- on p.product_id = o.product_id
+-- where EXTRACT(MONTH FROM order_date) = 2
+--   AND EXTRACT(YEAR FROM order_date) = 2020;
+-- group by product_name
+-- having sum(unit) >=100;
+
+select p.product_name as product_name ,
+ sum(o.unit) as unit
+from Products p inner join Orders o
 on p.product_id = o.product_id
-where EXTRACT(MONTH FROM order_date) = 2
-  AND EXTRACT(YEAR FROM order_date) = 2020;
-group by product_name
-having sum(unit) >=100;
+where o.order_date>='2020-02-01'
+AND o.order_date <'2020-03-01'
+group by p.product_name
+having sum(unit) >= 100
