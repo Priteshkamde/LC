897c16c6d636b9cd85df8c0a34802733d1728e04
Pritesh Kamde <40750377+Priteshkamde@users.noreply.github.com>
2025-01-02 18:26:31 -0700

    Time: 32 ms (73.85%), Space: 55.2 MB (25.86%) - LeetHub

--- 0630-course-schedule-iii/0630-course-schedule-iii.java
+++ 0630-course-schedule-iii/0630-course-schedule-iii.java
@@ -1,35 +1,24 @@
 class Solution {
     public int scheduleCourse(int[][] courses) {
         
-        Arrays.sort(courses, (a, b) -> Integer.compare(a[1], b[1]));
-
-        // max b element from [a,b] sort order
+        Arrays.sort(courses, (a,b) -> Integer.compare(a[1], b[1]));
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a,b) -> b-a);
+        int time = 0 ;
 
-        int currTime = 0;
-        
-        // iterate
-        for(int[] course : courses){
-           
-                // see if course can be completed within its last day
-                if(course[0] + currTime <= course[1]) {
-                    pq.offer(course[0]);
-                    currTime += course[0];
-                } else {
-            // check if to swap the biggest duration in our queue can be replaced by the new one
-                    if(pq.size() > 0 && course[0] < pq.peek()) {
-                        // remove the old biggest duration
-                        currTime -= pq.poll();
-                        // update time with the new biggest duration
-                        currTime += course[0];
-                        // add the new course to priority qqueue
-                        pq.offer(course[0]);
-                    }
-                
+        for(int[] course : courses) {
+
+            if(course[0] + time <= course[1]) {
+                time += course[0];
+                pq.offer(course[0]);
+            }
+            else if( pq.size() > 0 && pq.peek() > course[0] ) {
+                time -= pq.poll();
+                time += course[0];
+                pq.offer(course[0]);
             }
         }
-        
 
         return pq.size();
+
     }
 }
