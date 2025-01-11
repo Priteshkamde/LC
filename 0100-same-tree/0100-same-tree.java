633d78261f9b6384ffee9cca9ead06cfe41b1c9a
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-01-10 23:39:38 -0700

    Time: 0 ms (100%), Space: 41.6 MB (9.7%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -19,12 +19,9 @@ class Solution {
         if(p == null && q == null)
             return true;
     
-        if( p==null || q ==null )
+        if( p==null || q ==null || p.val != q.val  )
             return false;
-
-        if( p.val != q.val )
-            return false;
-
+            
         return
         isSameTree(p.left, q.left)
         &&
