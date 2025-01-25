c8f1942fd06ccd8c076e0ee025ca0f5ec271cd0a
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-01-25 00:10:35 -0700

    Time: 0 ms (100%), Space: 42.5 MB (58.94%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -17,24 +17,14 @@ class Solution {
             if(list1.val <= list2.val) { 
                 dummy.next = list1;
                 list1 = list1.next;
-                dummy = dummy.next;
             }
             else {
                 dummy.next = list2;
                 list2 = list2.next;
-                dummy = dummy.next;
             }
-        }
-        while(list1!= null) {
-            dummy.next = list1;
-            list1 = list1.next;
             dummy = dummy.next;
         }
-        while(list2!=null) {
-            dummy.next = list2;
-            list2 = list2.next;
-            dummy = dummy.next;
-        }
+        dummy.next = (list1 != null) ? list1 : list2;
 
         return head.next;
     }
