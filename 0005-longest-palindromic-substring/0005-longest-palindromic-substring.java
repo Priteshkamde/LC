dd5b3386c8b6bf5f7ff1373a4d000c5c2e36e0f5
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-01-29 23:38:06 -0700

    Time: 532 ms (17.75%), Space: 45.2 MB (41.93%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,43 +1,40 @@
 class Solution {
     public String longestPalindrome(String s) {
-        
+
         int N = s.length();
-        boolean[][] dp = new boolean[N][N];
+        if(N < 2) return s;
 
-        int maxLen = 1;
-        int start = 0;
+        int longest = 1;
+        String maxStr = s.substring(0,1);
 
-        for(int i = 0 ; i < N ; i++)
-            dp[i][i] = true; // diagonals are true
-
-        for(int i = 0 ; i < N-1 ; i++) { // for substring with len=2
-            if(s.charAt(i) == s.charAt(i+1)) {
-                dp[i][i+1] = true;
-                start = i;
-                maxLen = 2;
-            }
-        }
-
-        for (int k = 3; k <= N; ++k) {
-            for (int i = 0; i < N - k + 1; ++i) {
-                
-                int j = i + k - 1;
-                // a (bbaa) a = check if true within boundary
-                if (dp[i + 1][j - 1] && s.charAt(i) == s.charAt(j)) {
-                    dp[i][j] = true;
-                    // track k when its maximum
-                    // track len as k to extract the string
-                    if (k > maxLen) {
-                        start = i;
-                        maxLen = k;
-                    }
+        for(int i = 0 ; i < N; i++) {
+            for(int j = i + longest ; j <= N ; j++) {
+                if(
+                        j-i > longest && 
+                        isPalindrome(s.substring(i, j)) 
+                    ) {
+                    longest = j-i;
+                    maxStr = s.substring(i,j);
                 }
             }
         }
 
-        return s.substring(start, start + maxLen);
+        return maxStr;
+    }
 
-
+    private boolean isPalindrome(String s) {
         
+        int N = s.length();
+        int left = 0;
+        int right = N-1;
+
+        while(left < right) {
+            if(s.charAt(left) != s.charAt(right))
+                return false;
+
+            left++; 
+            right--;
+        }
+        return true;
     }
 }
