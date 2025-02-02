c872f417e8d87b9d7e6a2c805d113011c6dc428e
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-02-02 00:30:23 -0700

    Time: 2 ms (99.65%), Space: 44.5 MB (46.1%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,26 +1,19 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        
-    // using DP to keep a track
-    // we cannot have a increasing subseq 
-    // if the element X has a lower value then x+1..n
-    // DP array will keep a track starting from the end index
-    // reverse loop
-    
-        int N = nums.length;
-        int LIS[] = new int[N];
-        Arrays.fill(LIS, 1); //default is 1 since the number itself is a minimum seq
-          
-        LIS[N-1] = 1;
-        for (int i = N-1 ; i >= 0 ; i--)
-            for(int curr = i+1; curr < N ; curr++)
-                if(nums[i]<nums[curr])
-                    LIS[i] = Math.max(LIS[i],1+LIS[curr]);
-        
-        System.out.println(Arrays.toString(LIS));
-        return Arrays.stream(LIS).max().getAsInt();
-        
-        
-        // O(n^2)
+    int[] tails = new int[nums.length];
+    int size = 0;
+    for (int x : nums) {
+        int i = 0, j = size;
+        while (i != j) { // binary search for findin the index
+            int m = (i + j) / 2;
+            if (tails[m] < x)
+                i = m + 1;
+            else
+                j = m;
+        }
+        tails[i] = x;
+        if (i == size) ++size;
+    }
+    return size;
     }
 }
