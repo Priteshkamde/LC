c874791d69ddd1aa3bcc50e1c21e37cd463a495c
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-02-04 20:42:45 -0700

    Time: 2 ms (99.64%), Space: 43.8 MB (98.54%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,19 +1,24 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-    int[] tails = new int[nums.length];
-    int size = 0;
-    for (int x : nums) {
-        int i = 0, j = size;
-        while (i != j) { // binary search for findin the index
-            int m = (i + j) / 2;
-            if (tails[m] < x)
-                i = m + 1;
-            else
-                j = m;
+        int[] tails = new int[nums.length];
+        int size = 0;
+
+        for(int x : nums) {
+
+            int i = 0 ; int j = size;
+            while(i != j) {
+                int mid = (i+j)/2 ;
+
+                if(tails[mid] < x) // incoming number is greater try adding it in the tails since we need increasing seq
+                    i = mid + 1;
+                else 
+                    j = mid;
+            }
+            // add it to i'th index
+            tails[i] = x;
+            if ( i == size ) ++size;
         }
-        tails[i] = x;
-        if (i == size) ++size;
-    }
-    return size;
+
+        return size;
     }
 }
