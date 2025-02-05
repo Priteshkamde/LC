345dc2300ead4ddc7c59f233c0d769687d7aa1f0
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-02-04 23:36:44 -0700

    Time: 24 ms (86.84%), Space: 42.3 MB (86.07%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,32 +1,20 @@
 class Solution {
     public boolean canPartition(int[] nums) {
+        int n=nums.length,totalsum=Arrays.stream(nums).sum();
+
+        if(totalsum % 2 != 0)
+            return false;
         
-        int sum = Arrays.stream(nums).sum();
-        if( sum %2 != 0) 
-            return false;
+        int target = totalsum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
 
-        int index = 0;
-        return helperSum(nums, index, sum/2);
-    }
+        for (int num : nums) {
+            for (int j = target ; j >= num ; j--) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
 
-    Map<String, Boolean> map = new HashMap();
-
-    private boolean helperSum(int[] nums, int index, int currSum) {
-
-        if(currSum == 0) 
-            return true;
-        if(currSum < 0) 
-            return false;
-        if(index >= nums.length)
-            return false;
-
-        String key = currSum + "-" + index;
-
-        if(!map.containsKey(key)) 
-        map.put(key, 
-        helperSum(nums, index+1, currSum - nums[index]) || 
-        helperSum(nums, index+1, currSum));
-
-        return map.get(key);
+        return dp[target];
     }
 }
