5f835586ffa3b701a3a77fdb2b1c4a52719bd3ad
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-02-11 01:47:17 -0700

    Time: 7 ms (65.28%), Space: 47.9 MB (55.43%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,29 +1,14 @@
 class Solution {
-    
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> map = new HashMap<>();
-
-        for (int i = 0; i < strs.length; i++) {
-            // Convert the string to a character array
-            char[] charArray = strs[i].toCharArray();
-
-            Arrays.sort(charArray);
-            String sortedString = new String(charArray);
-
-            if (!map.containsKey(sortedString)) {
-            map.put(sortedString, new ArrayList<>());
-            }
-
+        Map<String, List<String>> resultMap = new HashMap();
         
-        map.get(sortedString).add(strs[i]);
-        }
-            
-        List<List<String>> listOfLists = new ArrayList<>();
-
-        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
-            listOfLists.add(entry.getValue());
+        for(String x : strs) {
+            char[] givenWord = x.toCharArray();
+            Arrays.sort(givenWord);
+            String sortedWord = new String(givenWord);
+            resultMap.computeIfAbsent(sortedWord, t -> new ArrayList()).add(x);
         }
 
-        return listOfLists;
+        return new ArrayList<>(resultMap.values());
     }
 }
