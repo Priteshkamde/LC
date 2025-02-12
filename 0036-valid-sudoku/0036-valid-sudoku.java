a79098a4a4f304d6d3e3227b3c71025cb2263486
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-02-12 13:51:32 -0700

    Time: 15 ms (12.06%), Space: 44.9 MB (25.77%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,16 +1,14 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        
-        HashSet set = new HashSet(9);
-        for(int i = 0 ; i < 9 ; i++){
-            for(int j = 0 ; j < 9 ; j++){
-                char currentValue = board[i][j]; 
-                if(currentValue != '.'){
-                    if( 
-                    !set.add(currentValue +" found in row" + i) ||
-                    !set.add(currentValue +" found in col" + j) ||
-                    !set.add(currentValue +" found in box" + i/3 + " "+ j/3)) 
-                    {
+
+        Set<String> set = new HashSet();
+
+        for(int i = 0 ; i < board.length ; i++) {
+            for(int j = 0 ; j < board[i].length ; j++) {
+                if(board[i][j] != '.') {
+                    if (!set.add(board[i][j] + " in row " + i) ||
+                        !set.add(board[i][j] + " in col " + j) ||
+                        !set.add(board[i][j] + " in box " + (i / 3) + "-" + (j / 3))) {
                         return false;
                     }
                 }
