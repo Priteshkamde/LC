4b5a21f41b4f3d5c5e030ec1b0c431d3957e1011
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-02-15 00:52:47 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        if(k == 0) return 0;
+
+        int left = 0;
+        int right = 0;
+        int maxFreq = 0;
+        int[] freq = new int[26];
+        int maxWindow = 0;
+
+        for(right = 0; right < s.length() ; right++) {
+            freq[s.charAt(right) - 'A']++;
+            maxFreq = Math.max(maxFreq, freq[s.charAt(right)-'A']);
+            int windowSize = right - left + 1;
+
+            if(windowSize - maxFreq > k) {
+                freq[s.charAt(left)-'A']--;
+                left++;
+            }
+
+            windowSize = right-left+1;
+            maxWindow = Math.max(maxWindow, windowSize);
+        }
+
+        return maxWindow;
+    }
+}
