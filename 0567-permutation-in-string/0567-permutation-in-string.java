12e14aebcf91f8be6d386e48749662c575e80a2f
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-02-15 15:55:45 -0700

    Time: 7 ms (72.7%), Space: 42.7 MB (69.06%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,32 +1,24 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        
+
         int[] first = new int[26];
         int[] second = new int[26];
 
-        for(char c : s1.toCharArray()){
-            first[c-'a']+=1;
+        for (char c : s1.toCharArray()) {
+            first[c - 'a'] += 1;
         }
 
-        int windowLen = s1.length()-1;
-        for(int i = 0 ; i < s2.length() ; i++) {
-            second[s2.charAt(i)-'a']+=1;
-            if(i >= windowLen){
-                if(match(first, second)){
-                    return true;
-                }
-                second[s2.charAt(i-windowLen)-'a']-=1;
+        for (int i = 0; i < s2.length(); i++) {
+            second[s2.charAt(i) - 'a'] += 1;
+            if (i >= s1.length())
+                second[s2.charAt(i - s1.length()) - 'a'] -= 1;
+
+            if (Arrays.equals(first, second)) {
+                return true;
+
             }
 
         }
         return false;
-    }
-
-    private boolean match(int[] first, int[] second) {
-        for(int i = 0 ; i < 26 ; i++)
-            if(first[i] != second[i])
-                return false;
-    
-        return true;
     }
 }
