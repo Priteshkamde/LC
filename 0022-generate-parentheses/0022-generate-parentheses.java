6f6d16531c30c994d4d2378c9193630482c857a8
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-02-16 16:41:22 -0700

    Time: 2 ms (50.38%), Space: 43.7 MB (16.52%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,19 +1,33 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        
-        List<String> res = new ArrayList<>();
-        backtrack(res, "", 0, 0, n);
-        return res;
+        // use backtrack // recur
+        // base case is to stop when output string length == n*2
+        // until that keep appending brackets
+        // helper(String output, String cur, openCount, closeCount, max)
+        List<String> output = new ArrayList<String>();
+        backtrack(output, "", 0, 0, n);
+        return output;
     }
-    
-    public void backtrack(List<String> res, String current_string, int open, int close, int max) {
-        if(current_string.length() == max * 2) {
-            res.add(current_string);
-            return;
-        }
-        
-        if(open < max) backtrack(res, current_string + "(", open+1, close, max);
-        if(close < open) backtrack(res, current_string + ")", open, close+1, max);
+
+    public void
+        backtrack(List<String> output, String currString, 
+            int open, int close, int max) {
+
+            // base case and 
+            // add something to the list 
+            if(currString.length() == max*2) {
+                output.add(currString);
+                return;
+            } 
+
+            // other cases will involve decision making
+                
+            if(open<max){
+                backtrack(output, currString+"(", open+1, close, max);
+            }
+            if(close<open){
+                backtrack(output, currString+")", open, close+1, max);
+            }
+         
     }
-    
 }
