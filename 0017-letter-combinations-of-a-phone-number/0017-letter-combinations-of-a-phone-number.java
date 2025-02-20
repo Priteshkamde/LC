f6956ed70864076b0dac2efa38734df0faee9c91
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-02-20 16:59:26 -0700

    Time: 0 ms (100%), Space: 41.9 MB (88.66%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -12,6 +12,8 @@ class Solution {
         return result;    
     }
 
+    // base case where we get a string char from initial stage and 
+    // append it with next letters in next number
     private void backtrack(int len, String digits, StringBuilder temp){
         if(len == digits.length()){
             result.add(temp.toString());
@@ -19,9 +21,16 @@ class Solution {
         }
 
         char ch = digits.charAt(len); 
+        // ch - '0' converts a character digit (e.g., '2') into an integer (2)
         String str = mapping[ch-'0'];
+        // "abc"
         for(char c : str.toCharArray()) {
+            // iterate using "a" from "abc"
             temp.append(c);
+            // increment length
+            // to move to next number in digits 
+            // extract "def"
+            // call backtrack with "a" + 
             backtrack(len+1, digits, temp);
             temp.deleteCharAt(temp.length()-1);
         }
