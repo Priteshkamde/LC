e42b0a06ef37a85b229c0c7486b5c075bac00389
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-02-20 18:17:36 -0700

    Time: 5 ms (33.81%), Space: 45 MB (55.71%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,53 @@
+class Solution {
+    List<List<String>> result = null;
+    public List<List<String>> solveNQueens(int n) {
+        result = new ArrayList();
+        char[][] board = new char[n][n];
+        // empty board
+        for(int i = 0 ; i < n ; i ++)
+            for(int j = 0 ; j < n ; j ++)
+                board[i][j] = '.';
+
+        // queens
+        List<int[]> queens = new ArrayList();
+        // DFS
+        // backtrack(start index, board[][], queens[])
+        backtrack(0, board, queens);
+        return result;
+    }
+
+    private void backtrack(int row, char[][] board, List<int[]> queens){
+        // base case, no. of queens = n
+        if(queens.size() == board.length){
+            List<String> rows = new ArrayList();
+            for(char[] r : board)
+                rows.add(new String(r));
+
+            // result
+            result.add(rows);
+        }
+
+        // try to add Queen
+        for(int col = 0 ; col < board.length ; col++) {
+            if(isSafe(row,col,queens)){
+                board[row][col] = 'Q';
+                queens.add(new int[]{row,col});
+                backtrack(row+1, board, queens);
+                // reset
+                board[row][col] = '.';
+                queens.remove(queens.size()-1);
+            }
+        }
+    }
+
+    private boolean isSafe(int row, int col, List<int[]> queens){
+        for(int[] current : queens) {
+            int dx = Math.abs(row - current[0]);
+            int dy = Math.abs(col - current[1]);
+            if(dx == 0 || dy == 0 || dx == dy)
+                return false;
+        }
+
+        return true;
+    }
+}
