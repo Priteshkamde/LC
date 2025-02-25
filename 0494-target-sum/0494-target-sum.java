488d0814a466e49b9f7fb6fba7e5c8b3f239be20
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-02-25 13:42:05 -0700

    Time: 64 ms (53.07%), Space: 45.1 MB (24.76%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        
+        Map<Integer, Integer> dp = new HashMap();
+        dp.put(0,1);
+        for(int num : nums) {
+
+            Map<Integer, Integer> nextdp = new HashMap();
+
+            for(int sum : dp.keySet()) {
+                int count = dp.get(sum);
+                // add
+                nextdp.put(
+                    sum+num, nextdp.getOrDefault(sum+num,0) + count
+                );
+                // subtract
+                nextdp.put(
+                    sum-num, nextdp.getOrDefault(sum-num,0) + count
+                );
+            }   
+            dp = nextdp;
+        }
+
+    return dp.getOrDefault(target, 0);
+    }
+}
