3309530deef37d3665c0b2ef2a95c2f754dd213e
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-02-24 19:44:39 -0700

    Time: 5 ms (64.46%), Space: 45.2 MB (30.47%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -4,21 +4,22 @@ class Solution {
         int n = word2.length();
         
         int[][] cost = new int[m + 1][n + 1];
-        for(int i = 0; i <= m; i++)
+
+        // base case empty string to another
+        for (int i = 0; i <= m; i++)
             cost[i][0] = i;
-        for(int i = 1; i <= n; i++)
-            cost[0][i] = i;
+        for (int j = 1; j <= n; j++)
+            cost[0][j] = j;
         
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(word1.charAt(i) == word2.charAt(j))
-                    cost[i + 1][j + 1] = cost[i][j];
-                else {
-                    int a = cost[i][j];
-                    int b = cost[i][j + 1];
-                    int c = cost[i + 1][j];
-                    cost[i + 1][j + 1] = a < b ? (a < c ? a : c) : (b < c ? b : c);
-                    cost[i + 1][j + 1]++;
+        // dp
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (word1.charAt(i) == word2.charAt(j)) {
+                    cost[i + 1][j + 1] = cost[i][j]; // if same character- no cost involved
+                } else {
+                    cost[i + 1][j + 1] = 1 + Math.min(
+                        cost[i][j], 
+                        Math.min(cost[i][j + 1], cost[i + 1][j]));
                 }
             }
         }
