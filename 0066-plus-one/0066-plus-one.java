a57d713eba84c6bd5d096bb0789abb87806ee6e7
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-03-06 00:46:34 -0700

    Time: 0 ms (100%), Space: 42.1 MB (29.92%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int N = digits.length;
+
+        for (int i = N - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i] += 1;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int result[] = new int[N + 1];
+        result[0] = 1;
+
+        return result;
+    }
+}
