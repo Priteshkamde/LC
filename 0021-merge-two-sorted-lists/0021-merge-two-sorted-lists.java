eef58cf14b581bc240a2e529301d30ef51e3518b
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-03-17 13:43:12 -0700

    Time: 0 ms (100%), Space: 42.5 MB (74.8%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,22 +10,39 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1 == null && list2 == null) return null;
-        ListNode dummy = new ListNode();
-        ListNode head = dummy;
-        while(list1 != null && list2 != null) {
-            if(list1.val <= list2.val) { 
-                dummy.next = list1;
-                list1 = list1.next;
+    
+        ListNode ptr1 = list1;
+        ListNode ptr2 = list2;
+        ListNode result = new ListNode();
+        ListNode newHead = result;
+        while(ptr1 != null && ptr2 != null) {
+            if(ptr1.val <= ptr2.val){
+                result.next = ptr1;
+                ptr1 = ptr1.next;
+                result = result.next;
+            } else {
+                result.next = ptr2;
+                ptr2 = ptr2.next;
+                result = result.next;
             }
-            else {
-                dummy.next = list2;
-                list2 = list2.next;
-            }
-            dummy = dummy.next;
         }
-        dummy.next = (list1 != null) ? list1 : list2;
 
-        return head.next;
+        while(ptr1 != null) {
+            result.next = ptr1;
+            ptr1 = ptr1.next;
+            result = result.next;
+        }
+
+        while(ptr2 != null) {
+            result.next = ptr2;
+            ptr2 = ptr2.next;
+            result = result.next;
+        }
+
+        return newHead.next;
+        // iterate on both AND
+        // select the smaller or equal values
+        // iterate on l1 (append)
+        // iterate on l2 (append)
     }
 }
