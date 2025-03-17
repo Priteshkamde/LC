1e6bf46597a7baf104db7d1646cb093e58c9f2b9
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-03-17 15:09:59 -0700

    Time: 0 ms (100%), Space: 42.1 MB (22.8%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -15,7 +15,7 @@ class Solution {
 
         ListNode slow = dummy;
         ListNode fast = dummy;
-        for(int i = 0 ; i < n ; i++){
+        for(int i = 1 ; i < n+1 ; i++){
             fast = fast.next;
         }
 
