009ff8b558df0e52d8a163815a98b5985dae8be0
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-03-18 13:26:44 -0700

    Time: 7 ms (99.32%), Space: 60.8 MB (6.71%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,13 +1,10 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-
         Set<Integer> set = new HashSet();
-
         for(int x : nums) 
             if(!set.add(x))
                 return true;
 
         return false;
-        
     }
 }
