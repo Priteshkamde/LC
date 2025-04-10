5cb0abacd4a21e59785fdde944cdaf7019c41cf9
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-04-10 00:52:45 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,23 +1,25 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        
-        HashSet<List<Integer>> result = new HashSet();
-
+        int N = nums.length;
         Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> temp;
+        int left = 0;
+        int right = 0;
 
         for(int i = 0 ; i < nums.length - 2; i++) {
-
-            int left = i+1;
-            int right = nums.length-1;
-
+            if(i>0 && nums[i] == nums[i-1]) 
+                continue;    
+            left = i+1;
+            right = N-1;
             while(left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if(sum == 0) {
-                    result.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                int currSum = nums[i]+nums[left]+nums[right];
+                
+                if(currSum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left++;
                     right--;
-                } else if (sum < 0){
+                } else if(currSum < 0) {
                     left++;
                 } else {
                     right--;
@@ -25,7 +27,6 @@ class Solution {
             }
         }
 
-        return new ArrayList(result);
-
+        return result;
     }
 }
