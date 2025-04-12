8560bb22e8fe6ec756bfcc8c3b6fb11c46136263
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-04-12 16:47:22 -0700

    Time: 0 ms (100%), Space: 41.6 MB (63.73%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,33 +1,40 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> result = new ArrayList();
+
+        List<Integer> result = new ArrayList<>();
         int rowEnd = matrix.length - 1;
         int colEnd = matrix[0].length - 1;
         int rowBegin = 0;
         int colBegin = 0;
         while (rowBegin <= rowEnd && colBegin <= colEnd) {
-            // towards right
-            for (int j = colBegin; j <= colEnd; j++)
+            
+            for(int j = colBegin ; j <= colEnd ; j++) {
                 result.add(matrix[rowBegin][j]);
-
-            // down
+            }
+            
             rowBegin++;
-            for (int i = rowBegin; i <= rowEnd; i++)
-                result.add(matrix[i][colEnd]);
+            for(int j = rowBegin; j <= rowEnd ; j++) {
+                result.add(matrix[j][colEnd]);
+            }
 
-            // towards left
+            //left
             colEnd--;
-            if (rowBegin <= rowEnd)
-                for (int x = colEnd; x >= colBegin; x--)
-                    result.add(matrix[rowEnd][x]);
-            // up
+            if(rowBegin <= rowEnd)
+                for(int j = colEnd; j >= colBegin ; j--) {
+                    result.add(matrix[rowEnd][j]);
+                }
+
+            //up
             rowEnd--;
-            if (colBegin <= colEnd)
-                for (int x = rowEnd; x >= rowBegin; x--)
-                    result.add(matrix[x][colBegin]);
+            if(colBegin <= colEnd)
+                for(int j = rowEnd; j >= rowBegin ; j--) {
+                    result.add(matrix[j][colBegin]);
+                }
 
             colBegin++;
         }
+
         return result;
+
     }
 }
