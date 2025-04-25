30994018292dd218904894888195e9ff0668cbea
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-04-25 16:30:46 -0700

    Time: 5 ms (25.62%), Space: 45.5 MB (42.85%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,15 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int rangeTotal = 0;
-        int currTotal = 0;
-        for(int x : nums) {
-            currTotal += x; 
-        }
+        Arrays.sort(nums);
 
-        for(int i = 0 ; i <= nums.length ; i++) {
-            rangeTotal += i;
+        int left = 0, right = nums.length-1;
+        while(left<=right){
+            int mid = (left + right)/2;
+            if(nums[mid]>mid) right = mid-1;
+            else left = mid+1;
         }
-
-        return rangeTotal - currTotal;
+        return left;
     }
 }
