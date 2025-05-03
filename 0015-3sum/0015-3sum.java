802c8e226267a215f21854ee711b19a9ab5bc1dc
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-05-02 19:07:16 -0700

    Time: 30 ms (69.11%), Space: 51.6 MB (59.95%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,32 +1,39 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int N = nums.length;
+        List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> temp;
-        int left = 0;
-        int right = 0;
+        int N = nums.length;
+        
+        for(int i = 0 ; i < N-2 ; i++) {
+            if(i > 0 && nums[i] == nums[i-1])
+                continue;
+            
+            int left = i+1;
+            int right = N-1;
 
-        for(int i = 0 ; i < nums.length - 2; i++) {
-            if(i>0 && nums[i] == nums[i-1]) 
-                continue;    
-            left = i+1;
-            right = N-1;
             while(left < right) {
-                int currSum = nums[i]+nums[left]+nums[right];
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0) {
+                    list.add(Arrays.asList(nums[i], nums[left], nums[right]));
                 
-                if(currSum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while(left < right && nums[left] == nums[left+1]) {
+                        left++;
+                    }
+                    while(left < right && nums[right] == nums[right-1]) {
+                        right--;
+                    }
+
                     left++;
                     right--;
-                } else if(currSum < 0) {
-                    left++;
+                }
+                else if(sum < 0) {
+                    left += 1;
                 } else {
-                    right--;
+                    right -= 1;
                 }
             }
         }
 
-        return result;
+        return list;
     }
 }
