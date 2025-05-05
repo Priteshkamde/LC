ecbe07eb0fa83651c8f33bf9d34dc05473204d60
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-05-04 23:39:32 -0700

    Time: 3 ms (79.93%), Space: 45 MB (32.27%) - LeetHub

--- 0429-n-ary-tree-level-order-traversal/0429-n-ary-tree-level-order-traversal.java
+++ 0429-n-ary-tree-level-order-traversal/0429-n-ary-tree-level-order-traversal.java
@@ -16,8 +16,35 @@ class Node {
     }
 };
 */
-
+// Tc - O(n)
 class Solution {
+    // public List<List<Integer>> levelOrder(Node root) {
+    //     if (root == null)
+    //         return new ArrayList<>();
+
+    //     Queue<Node> q = new LinkedList<>();
+    //     List<List<Integer>> result = new ArrayList<>();
+    //     List<Integer> list;
+    //     q.add(root);
+
+    //     while (!q.isEmpty()) {
+    //         int size = q.size();
+    //         list = new ArrayList(); 
+    //         for (int i = 0; i < size; i++) {
+    //             Node n = q.poll();
+    //             list.add(n.val);
+    //             if(n.children != null) {
+    //                 for(Node child : n.children) {
+    //                     q.add(child);
+    //                 }
+    //             }
+    //         }
+    //         result.add(list);
+    //     }
+
+    //     return result;
+    // }
+    // rec
     public List<List<Integer>> levelOrder(Node root) {
         if (root == null)
             return new ArrayList<>();
