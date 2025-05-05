d8681f588d4f7969d690314261d02baa6172a099
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-05-05 16:31:01 -0700

    Time: 24 ms (87.51%), Space: 42.6 MB (87.26%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -19,20 +19,26 @@ class Node {
 */
 
 class Solution {
-    
-    Map<Node,Node> map = new HashMap<>();
-    
-    public Node cloneGraph(Node node) {
-        
-        if(node ==null) return null;
-        
-        Node curr = new Node(node.val);
-        map.put(node, curr);
-        for(Node nei : node.neighbors) {
-            if(!map.containsKey(nei))
-                cloneGraph(nei);
-            curr.neighbors.add(map.get(nei));
+    private Node cloneUtil(Node node, Map<Node, Node> map) {
+        // create a newNode with input value
+        Node newNode = new Node(node.val);
+        map.put(node, newNode);
+        for(Node neighbor : node.neighbors) {
+            if(!map.containsKey(neighbor)) {
+                // not cloned and create a new one here
+                newNode.neighbors.add(cloneUtil(neighbor, map));
+            } else {
+                // its already cloned, so only append its neighbors by pulling from hashmap
+                newNode.neighbors.add(map.get(neighbor));
+            }
         }
-        return curr;
+        return newNode;
+    }
+
+    public Node cloneGraph(Node node) {
+        if( node == null )
+            return null;
+        Map<Node, Node> map = new HashMap<>();
+        return cloneUtil(node, map);
     }
 }
