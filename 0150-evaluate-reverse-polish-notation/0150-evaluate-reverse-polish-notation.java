c1fc2a15c11aa4c6a02d65de3c3a2b09983885a3
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-05-05 17:14:46 -0700

    Time: 6 ms (72.78%), Space: 44.7 MB (67.92%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,41 +1,32 @@
 class Solution {
     public int evalRPN(String[] tokens) {
 
-        Stack<Integer> stack = new Stack();
-
-        for (String token : tokens) {
-            if (isNumber(token))
-                stack.push(Integer.parseInt(token));
-
-            else {
+        Stack<Integer> stack = new Stack<>();
+        Set<String> symbols = Set.of("+", "-", "*", "/");
+        for (String s : tokens) {
+            if (!symbols.contains(s)) {
+                stack.push(Integer.parseInt(s));
+            } else {
                 int a = stack.pop();
                 int b = stack.pop();
-                switch (token) {
-                    case "+":
-                        stack.push(a + b);
-                        break;
-                    case "-":
-                        stack.push(b - a);
-                        break;
+
+                switch(s)
+                {
                     case "*":
-                        stack.push(a * b);
-                        break;
+                    stack.push(a * b); break;
+
+                    case "-":
+                    stack.push(b - a); break;
+
+                    case "+":
+                    stack.push(a + b); break;
+
                     case "/":
-                        stack.push(b / a);
-                        break;
+                    stack.push(b / a); break;
                 }
             }
         }
 
-        return stack.pop();
-    }
-
-    private boolean isNumber(String token) {
-        try {
-            Integer.parseInt(token);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
+        return stack.peek();
     }
 }
