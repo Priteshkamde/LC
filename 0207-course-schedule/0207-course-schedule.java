c0f434892dc1f25bddb8d9a58b9c02e7e378dcea
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-05-05 22:14:20 -0700

    Time: 6 ms (74.51%), Space: 45.8 MB (28.88%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,45 +1,47 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        
+        // create adj matrix
+        // calculate indegree
+        // select the indegree where it is zero, and add to queue
+        // start with the queue
+        // add to queue only if indegree is zero, means it fully explored
+        // last check is numCourses size is same as Topo result
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>(); 
+
         for(int i = 0 ; i < numCourses ; i++) {
             adj.add(new ArrayList<>());
         }
 
-        for(int[] edge : prerequisites) {
-            adj.get(edge[1]).add(edge[0]);
+        for(int[] preq : prerequisites) {
+            adj.get(preq[1]).add(preq[0]);
         }
 
         int[] indegree = new int[numCourses];
-        for (int i = 0; i < numCourses; i++) {
-            for (int edge : adj.get(i)) {
-                indegree[edge]++;
+        for(int i = 0 ; i < numCourses ; i++) {
+            for(int incoming : adj.get(i)) {
+                indegree[incoming]++;
             }
         }
 
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
+        Queue<Integer> q = new LinkedList<Integer>();
+        for(int i = 0 ; i < indegree.length ; i++) {
+            if(indegree[i] == 0)
                 q.add(i);
-            }
         }
 
-        List<Integer> toposort = new ArrayList<>();
-        
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            toposort.add(node);
+        int count = 0;
+        while(!q.isEmpty()) {
+            int curr = q.poll();
+            count+=1;
 
-            for (int neighbor : adj.get(node)) {
+            for(int neighbor : adj.get(curr)) {
                 indegree[neighbor]--;
-                if (indegree[neighbor] == 0) {
+                if(indegree[neighbor] == 0)
                     q.add(neighbor);
-                }
             }
-        }
+        }   
 
-
-        return toposort.size() == numCourses;
-
+        return count == numCourses;
     }
 }
