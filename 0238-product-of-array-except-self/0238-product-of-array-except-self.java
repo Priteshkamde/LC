5308de9efbe3807b4a3383912573ad440e7f49b1
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-05-06 17:21:20 -0700

    Time: 2 ms (88.33%), Space: 56.4 MB (16.08%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,21 +3,20 @@ class Solution {
         int N = nums.length;
         int[] prefix = new int[N];
         int[] suffix = new int[N];
+        int[] result = new int[N];
 
         prefix[0] = 1;
-        System.out.println(Arrays.toString(prefix));
-        for(int i = 1 ; i < N ; i++) {
-            prefix[i] = nums[i-1] * prefix[i-1];
-        }
-        System.out.println(Arrays.toString(prefix));
         suffix[N-1] = 1;
-        System.out.println(Arrays.toString(suffix));
-        for(int i = N-2 ; i >= 0 ; i--) {
-            suffix[i] =  nums[i+1] * suffix[i+1];
+
+        for(int i = 1 ; i < N ; i++) {
+            prefix[i] = prefix[i-1] * nums[i-1];
         }
-        System.out.println(Arrays.toString(suffix));
-        int[] result = new int[N];  
-        for(int i = 0 ; i < N ; i ++) {
+
+        for(int i = N-2 ; i >= 0 ; i--) {
+            suffix[i] = nums[i+1] * suffix[i+1];
+        }
+
+        for(int i = 0 ; i < N ; i++) {
             result[i] = prefix[i] * suffix[i];
         }
 
