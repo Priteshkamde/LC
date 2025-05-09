ae45dd2e39566870f8fe7be0a4807a92d9bec021
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-05-09 00:31:06 -0700

    Time: 1 ms (90.3%), Space: 45.1 MB (44.5%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,26 +1,27 @@
 class Solution {
     public int maxProduct(int[] nums) {
+        if(nums.length == 0)    
+            return 0;
 
-        // nums array
-        // have a left ptr inc
-        // right ptr dec
-        // caulculate the product with the follwing
-        // problem when we encounter a zero // we reset the curr product to 1
+        if(nums.length == 1)
+            return nums[0];
 
         int N = nums.length;
+
         int leftProduct = 1;
         int rightProduct = 1;
-        int max = -99;
+        int max = Integer.MIN_VALUE;
         for(int i = 0 ; i < N ; i++) {
-
-            if(leftProduct == 0) leftProduct = 1;
-            if(rightProduct == 0) rightProduct = 1;
-
+            if(leftProduct == 0) 
+                leftProduct = 1;
+            if(rightProduct == 0) 
+                rightProduct = 1;
+                
             leftProduct = leftProduct * nums[i];
-            rightProduct = rightProduct * nums[N-i-1];
-
+            rightProduct = rightProduct * nums[N-1-i];
             max = Math.max(max, Math.max(leftProduct, rightProduct));
         }
+
         return max;
     }
 }
