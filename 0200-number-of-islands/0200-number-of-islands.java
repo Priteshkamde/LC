10e4e282be182f6b6cc4a772007a23332b76abbe
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-05-08 18:44:06 -0700

    Time: 3 ms (84.15%), Space: 49.6 MB (52.8%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,40 +1,30 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        
-        int total = 0;
-        int rows = grid.length; 
-        int cols = grid[0].length; 
-
-        for(int r = 0 ; r < rows ; r++){
-            for(int c = 0 ; c < cols ; c++){
-                if(grid[r][c] == '1'){
-                    total += 1;
-                    callBFS(r,c,grid,rows, cols);
-                }
-            }
-        }   
-        return total;
-    }
-    public void callBFS(int r, int c, char[][] grid, int rows, int cols){
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{r,c});
-        grid[r][c] = 0 ;
-
-        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
-
-        while(!q.isEmpty()){
-            int[] point = q.poll();
-            int row = point[0], col = point[1];
-
-            for(int[] dir : directions) {
-                int newRow = row + dir[0];
-                int newCol = col + dir[1];
-                if(newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols 
-                && grid[newRow][newCol] == '1') {
-                    q.add(new int[]{newRow, newCol});
-                    grid[newRow][newCol] = '0';
+        int row = grid.length;
+        int col = grid[0].length;
+        boolean[][] visited = new boolean[row][col];
+        int islands = 0;
+        for(int i = 0 ; i < row ; i++) {
+            for(int j = 0 ; j < col ; j++) {
+                if( grid[i][j] == '1') {
+                    dfs(i,j,row, col, grid, visited);
+                    islands+=1;
                 }
             }
         }
+
+        return islands;
+    }
+
+    void dfs(int i,int j,int row, int col, char[][] grid, boolean[][] visited) {
+
+        if(i<0 || j < 0 || i >= row || j >= col || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(i+1, j, row, col, grid, visited);
+        dfs(i-1, j, row, col, grid, visited);
+        dfs(i, j+1, row, col, grid, visited);
+        dfs(i, j-1, row, col, grid, visited);
     }
 }
