aa17a209acf22fe28edc00f2c57742112c4e66b0
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-05-12 01:01:34 -0700

    Time: 0 ms (100%), Space: 42.5 MB (62.98%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -10,23 +10,41 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head == null || head.next == null || k == 0) return head;
+        if(head == null) 
+            return null;
 
-        ListNode curr = head;
-        int len = 1;
-        while(curr.next != null) {
-            curr = curr.next;
-            len++;
+        if(head.next == null)
+            return head;
+            
+        if(k == 0)
+            return head;
+
+        int lenCount = 1;
+        ListNode ptr = head;
+        while(ptr.next != null) {
+            lenCount++;
+            ptr = ptr.next;
         }
 
-        curr.next = head;
-        k = len - k % len;
+        if(k % lenCount == 0)
+            return head;
 
-        while(k-- > 0) curr = curr.next;
+        k = k % lenCount;   
+        ptr.next = head; // join the last section
+        // at this point the LL is singly-cicular
 
-        head = curr.next;
-        curr.next = null;
+        int split = lenCount - k;
+        int i = 1;
+        ListNode ctr = head;
+        while(i < split) {
+            i++;
+            ctr = ctr.next;
+        }
+
+        head = ctr.next;
+        ctr.next = null;
 
         return head;
+
     }
 }
