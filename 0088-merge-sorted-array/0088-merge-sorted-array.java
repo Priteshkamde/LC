c8b2c20a9651b0502c4a945b6bf1fa3739e0a7b1
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-05-19 14:24:24 -0700

    Time: 0 ms (100%), Space: 42.4 MB (42.69%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,28 +1,31 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        
-        // to get last index of nums1
-        int lastIndex = m + n - 1;
-        
-        int index1 = m - 1;
-        int index2 = n - 1;
+        if(n == 0)
+            return;
+        if(m == 0 && n ==0)
+            return;
 
-        
         if(m == 0) 
         {
-            for(int i = 0; i < n; i++) nums1[i] = nums2[i];
+            for(int i = 0; i < n; i++) 
+                nums1[i] = nums2[i];
             return ;
         }
-        while(index2 >= 0){
-            
-            if(index1 >= 0 && nums1[index1] > nums2[index2]) {
-                nums1[lastIndex--] = nums1[index1--]; // side by side copy withing nums1 element
+
+        int writeIndex = m+n-1;
+        int bptr = n-1;
+        int aptr = m-1;
+
+        while (bptr >= 0) {
+            if (aptr >= 0 && nums1[aptr] > nums2[bptr]) {
+                nums1[writeIndex] = nums1[aptr];
+                aptr--;
+            } else {
+                nums1[writeIndex] = nums2[bptr];
+                bptr--;
             }
-            
-            else {
-                nums1[lastIndex--] = nums2[index2--]; // move to nums1 from nums2
-            }
-            
-        }               
-        } 
+            writeIndex--;
+        }
+        
     }
+}
