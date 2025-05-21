3416861ed72663c00d89665f43f5c737eb777d40
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-05-21 10:51:56 -0700

    Time: 2 ms (97.23%), Space: 41.8 MB (65.95%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,24 +3,26 @@ class Solution {
         
         Stack<Character> stack = new Stack<>();
 
-        for(char c : s.toCharArray()) {
-            if(c == '(' || c == '[' || c == '{') {
-                stack.push(c);
+        for(int i = 0 ; i < s.length() ; i++) {
+            char ch = s.charAt(i);
+
+            if(ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            }
+            else if(stack.isEmpty()) {
+                return false;
             }
             else {
-                if(stack.isEmpty())
-                    return false;
-                
                 char popChar = stack.pop();
-                    if(c == ')' && popChar != '(')
-                        return false;
-                    if(c == ']' && popChar != '[')
-                        return false;
-                    if(c == '}' && popChar != '{')
-                        return false;
-                
+                if(popChar == '(' && ch != ')')
+                    return false;
+                if(popChar == '{' && ch != '}')
+                    return false;
+                if(popChar == '[' && ch != ']')
+                    return false;
             }
         }
+
         return stack.isEmpty();
     }
 }
