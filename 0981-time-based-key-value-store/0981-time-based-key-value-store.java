0a1506acb215ec25dc0efd68f6bfbce3d64a15c5
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-05-21 13:16:39 -0700

    Time: 135 ms (82.63%), Space: 118.7 MB (76.49%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.java
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -1,47 +1,52 @@
-class TimeMap {
-
-    class Data {
-        String value;
-        int timestamp;
-        public Data(String value, int timestamp){
-            this.timestamp = timestamp;
-            this.value = value;
-        }
+class Data {
+    String value;
+    int timeStamp;
+    public Data(String value, int timeStamp) {
+        this.value = value;
+        this.timeStamp = timeStamp;
     }
+}
 
-    HashMap<String, List<Data>> map;
+class TimeMap {
+    Map<String, List<Data>> map;
 
     public TimeMap() {
-        map = new HashMap();
+        map = new HashMap<>();
     }
     
     public void set(String key, String value, int timestamp) {
-        if(!map.containsKey(key)) map.put(key, new ArrayList<Data>());
-        map.get(key).add(new Data(value, timestamp));
+        Data data = new Data(value, timestamp);
+        map.putIfAbsent(key, new ArrayList<>());
+        map.get(key).add(data);
     }
     
     public String get(String key, int timestamp) {
-        if(!map.containsKey(key)) return "";
+        if(!map.containsKey(key))
+            return "";
 
-        List<Data> list = map.get(key);
+        List<Data> retrievedList = map.get(key);
+        String s = find(retrievedList, timestamp);
 
-        return helper(list, timestamp);
+        return s;
     }
 
-    public String helper(List<Data> list, int timestamp){
-        // binary search in the list
+    public String find(List<Data> list, int inputTimestamp) {
         int low = 0;
         int high = list.size()-1;
+        String res = "";
+        int mid = 0;
+        while(low<=high) {
+            mid = low + (high-low)/2;
 
-        while(low < high) {
-            int mid = (low + high + 1)/2;
-            if(list.get(mid).timestamp <= timestamp) {
-                low = mid;
-            }  else {
-                high = mid - 1;
+            if(list.get(mid).timeStamp <= inputTimestamp) {
+                res = list.get(mid).value; // value extract here
+                low = mid + 1;
+            } else {
+                high = mid-1;
             }
         }
-        return list.get(low).timestamp > timestamp ? "" : list.get(low).value;
+        
+        return res;
     }
 }
 
