bfa2a3fddf90af1b7fd7c6d2e862f3071af93038
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-05-28 23:10:30 -0700

    Time: 8 ms (86.81%), Space: 46.2 MB (98.5%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,24 +1,24 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-
-        List<int[]> list = new ArrayList<>();
+        List<int[]> result = new ArrayList<>();
 
         Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
 
-        int[] current = intervals[0];
+        int[] newInterval = intervals[0];
+        result.add(intervals[0]);
 
-        for(int i = 0; i < intervals.length ; i++) {
-            int[] next = intervals[i];
-
-            if(current[1] >= next[0]) {
-                current[1] = Math.max(current[1], next[1]);
-            } else {
-                list.add(current);
-                current = next;
+        for(int[] interval : intervals) {
+            // overlap
+            if(interval[0] <= newInterval[1]) {
+                newInterval[1] = Math.max(newInterval[1], interval[1]);
+            } 
+            // no overlap
+            else {
+                newInterval = interval;
+                result.add(newInterval);
             }
         }
-        list.add(current);
 
-        return list.toArray(new int[list.size()][2]);
+        return result.toArray(new int[result.size()][2]);
     }
 }
