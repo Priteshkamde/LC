74b0e63347e4055bbc32b41f15ccbbaa996126a6
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-06-19 15:52:49 -0700

    Time: 15 ms (86.34%), Space: 46.4 MB (21.47%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -5,39 +5,34 @@ class Pair {
         this.c = c;
         this.freq = freq;
     }
-    int getFreq(char c) {
-        return freq;
-    }
 }
 
 class Solution {
     public String removeDuplicates(String s, int k) {
-        Stack<Pair> stack = new Stack<>();
+        ArrayDeque<Pair> stack = new ArrayDeque<>();
         for(Character ch : s.toCharArray()) {
             
             // if equals -> finding adjacents
-            if(!stack.isEmpty() && stack.peek().c == ch) {
-                stack.peek().freq++;
+            if(!stack.isEmpty() && stack.peekLast().c == ch) {
+                stack.peekLast().freq++;
         
-                if(stack.peek().freq == k) {
-                    stack.pop();
+                if(stack.peekLast().freq == k) {
+                    stack.removeLast();
                 }
             }
             // else -> not adjacenty 
             else {
-                stack.push(new Pair(ch, 1));
-            }   
+                stack.addLast(new Pair(ch, 1));
+            }
         }
 
         // stack either empty || Pair <a,4>
         StringBuilder sb = new StringBuilder();
         while(!stack.isEmpty()) {
             Pair p = stack.pop();
-            while(p.freq-- > 0) {
-                sb.append(p.c);
-            }
+            sb.append(String.valueOf(p.c).repeat(p.freq));
         }
 
-        return sb.reverse().toString();
+        return sb.toString();
     }
 }
