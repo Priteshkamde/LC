30a4948b66b13974bebf36e2a17e04e56f34084c
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-06-19 15:30:48 -0700

    Time: 31 ms (52.38%), Space: 45.4 MB (83.66%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,39 +1,43 @@
-class Solution {
-    public String removeDuplicates(String s, int k) {
-
-        ArrayDeque<Pair> stack = new ArrayDeque<>();
-        for (char ch : s.toCharArray()) {
-            if (!stack.isEmpty() && stack.peek().c == ch) {
-                stack.peek().count++;
-
-                if (stack.peek().count == k) {
-                    stack.pop();
-                }
-            } else {
-                stack.push(new Pair(ch, 1));
-            }
-        }
-
-        StringBuilder sb = new StringBuilder();
-        while (!stack.isEmpty()) {
-            Pair p = stack.removeLast();
-            int count = p.count;
-            while (count > 0) {
-                sb.append(p.c);
-                count--;
-            }
-        }
-
-        return sb.toString();
+class Pair {
+    char c;
+    int freq;   
+    Pair(char c, int freq) {
+        this.c = c;
+        this.freq = freq;
+    }
+    int getFreq(char c) {
+        return freq;
     }
 }
 
-class Pair {
-    char c;
-    int count;
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        Stack<Pair> stack = new Stack<>();
+        for(Character ch : s.toCharArray()) {
+            
+            // if equals -> finding adjacents
+            if(!stack.isEmpty() && stack.peek().c == ch) {
+                stack.peek().freq++;
+        
+                if(stack.peek().freq == k) {
+                    stack.pop();
+                }
+            }
+            // else -> not adjacenty 
+            else {
+                stack.push(new Pair(ch, 1));
+            }   
+        }
 
-    Pair(char c, int count) {
-        this.c = c;
-        this.count = count;
+        // stack either empty || Pair <a,4>
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()) {
+            Pair p = stack.pop();
+            while(p.freq-- > 0) {
+                sb.append(p.c);
+            }
+        }
+
+        return sb.reverse().toString();
     }
 }
