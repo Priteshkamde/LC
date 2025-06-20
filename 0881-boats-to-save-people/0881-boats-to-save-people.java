9cb3f157bf64b6ee9a0619a15a763eafab3a29de
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-06-19 17:47:50 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+
+        int boxes = 0;
+
+        int low = 0;
+        int high = people.length-1;
+        
+        while(low <= high) {
+            if(people[low] + people[high] > limit) {
+                boxes += 1;
+                high -= 1;
+            } else {
+                boxes += 1;
+                low += 1;
+                high -= 1;
+            }
+        }
+
+        re
+        turn boxes;
+    }
+}
