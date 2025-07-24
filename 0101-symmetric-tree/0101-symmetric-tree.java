c11e5940392e1a482668aeaffc9fad6466085e74
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-07-24 00:56:15 -0700

    Time: 0 ms (100%), Space: 42.1 MB (19.64%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -23,12 +23,13 @@ class Solution {
     }
 
     boolean isSameTree(TreeNode a, TreeNode b) {
-        if(a == null && b == null) 
+        
+        if(a == null && b==null)
             return true;
 
-        if(a == null || b == null) 
+        if(a == null || b == null)
             return false;
-        
+
         if(a.val != b.val)
             return false;
 
