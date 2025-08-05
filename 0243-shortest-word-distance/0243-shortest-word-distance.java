670a5446e5cd8ebd79938adad5d69b80fadfcb8e
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-08-05 13:19:26 -0700

    Time: 2 ms (93.17%), Space: 46 MB (75.98%) - LeetHub

---
+++ 0243-shortest-word-distance/0243-shortest-word-distance.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int shortestDistance(String[] wordsDict, String word1, String word2) {
+        
+        int index1 = -1; 
+        int index2 = -1;
+        int dist = Integer.MAX_VALUE;
+
+        for(int i = 0; i < wordsDict.length ; i++) {
+            if(wordsDict[i].equals(word1)) 
+                index1 = i;
+            if(wordsDict[i].equals(word2)) 
+                index2 = i;
+
+            if(index1 != -1 && index2 != -1)
+                dist = Math.min(dist, Math.abs(index1 - index2));
+        }
+
+        return dist;
+    }
+}
