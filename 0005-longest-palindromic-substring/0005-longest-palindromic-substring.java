54e661e9775a0abe70992677437c29332c2094b1
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-08-10 12:46:49 -0700

    Time: 16 ms (62.44%), Space: 44.7 MB (50.04%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -7,24 +7,25 @@ class Solution {
         int start = 0;
         int end = 0;
         for(int i = 0 ; i < s.length(); i++) {
-            int oddLen = expand(s, i, i);
-            int evenLen = expand(s, i, i+1);
+            int[] oddLen = expand(s, i, i);
+            int[] evenLen = expand(s, i, i+1);
 
-            int len = Math.max(oddLen, evenLen);
-            if(len > end-start) {
-                start = i - (len-1)/2;
-                end = i + len/2;
+            int maxLen[] = (oddLen[1] - oddLen[0]) > (evenLen[1] - evenLen[0]) ? oddLen : evenLen;
+
+            if(maxLen[1] - maxLen[0] > end-start) {
+                start = maxLen[0];
+                end =  maxLen[1];
             }
         }
 
         return s.substring(start, end+1);
     }
 
-    int expand(String s, int start, int end) {
+    int[] expand(String s, int start, int end) {
         while(start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {
                 start--;
                 end++;
         }
-        return end-start-1;
+        return new int[]{start+1, end-1};
     }
 }
