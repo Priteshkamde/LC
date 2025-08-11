5c93af03897fc05e2e0c668367089463021e3b2f
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-08-10 23:43:21 -0700

    Time: 6 ms (73.02%), Space: 45.8 MB (26.29%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,47 +1,43 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        // create adj matrix
-        // calculate indegree
-        // select the indegree where it is zero, and add to queue
-        // start with the queue
-        // add to queue only if indegree is zero, means it fully explored
-        // last check is numCourses size is same as Topo result
 
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>(); 
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
 
-        for(int i = 0 ; i < numCourses ; i++) {
-            adj.add(new ArrayList<>());
-        }
+        for (int i = 0; i < numCourses; i++)
+            adjList.add(new ArrayList<>());
 
-        for(int[] preq : prerequisites) {
-            adj.get(preq[1]).add(preq[0]);
+        for (int[] prereq : prerequisites) {
+            adjList.get(prereq[1]).add(prereq[0]);
         }
 
         int[] indegree = new int[numCourses];
-        for(int i = 0 ; i < numCourses ; i++) {
-            for(int incoming : adj.get(i)) {
-                indegree[incoming]++;
+        for (int i = 0; i < numCourses; i++) {
+            for (int neighbor : adjList.get(i)) {
+                indegree[neighbor]++;
             }
         }
 
         Queue<Integer> q = new LinkedList<Integer>();
-        for(int i = 0 ; i < indegree.length ; i++) {
-            if(indegree[i] == 0)
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
                 q.add(i);
+            }
+        }
+        int count = 0;
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            count++;
+
+            for (int neighbor : adjList.get(node)) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.add(neighbor);
+                }
+            }
         }
 
-        int count = 0;
-        while(!q.isEmpty()) {
-            int curr = q.poll();
-            count+=1;
-
-            for(int neighbor : adj.get(curr)) {
-                indegree[neighbor]--;
-                if(indegree[neighbor] == 0)
-                    q.add(neighbor);
-            }
-        }   
-
         return count == numCourses;
+
     }
 }
