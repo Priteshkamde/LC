1a33f32fa3d8ea287435e075348584040758f5a1
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-08-11 01:02:18 -0700

    Time: 15 ms (76.51%), Space: 44.1 MB (90.09%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,20 +1,20 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int dp[] = new int[amount + 1];
-        Arrays.fill(dp, amount + 1); // pre set with a higher amt 
-        dp[0] = 0; // amt = 0
 
-        // iterate thru 1 -> amount
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+
         for (int i = 1; i <= amount; i++) {
-            // iterate thru the coins
             for (int coin : coins) {
-                // ensure the coin < the amount to be built
                 if (coin <= i) {
                     dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
                 }
             }
         }
 
-        return dp[amount] > amount ? -1 : dp[amount]; // -1 if no solution
+        if (dp[amount] > amount)
+            return -1;
+        return dp[amount];
     }
 }
