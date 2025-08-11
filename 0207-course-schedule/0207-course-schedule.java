e66ae415be21d00c3510561bd654e288335d6d2a
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-08-11 00:24:23 -0700

    Time: 5 ms (79.04%), Space: 44.7 MB (98.57%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,43 +1,37 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
 
-        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
 
         for (int i = 0; i < numCourses; i++)
-            adjList.add(new ArrayList<>());
-
-        for (int[] prereq : prerequisites) {
-            adjList.get(prereq[1]).add(prereq[0]);
-        }
+            adj.add(new ArrayList<>());
 
         int[] indegree = new int[numCourses];
-        for (int i = 0; i < numCourses; i++) {
-            for (int neighbor : adjList.get(i)) {
-                indegree[neighbor]++;
-            }
+
+        for (int[] preq : prerequisites) {
+            adj.get(preq[1]).add(preq[0]);
+            indegree[preq[0]]++;
         }
 
         Queue<Integer> q = new LinkedList<Integer>();
         for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
+            if (indegree[i] == 0)
                 q.add(i);
-            }
         }
+
         int count = 0;
 
         while (!q.isEmpty()) {
             int node = q.poll();
             count++;
 
-            for (int neighbor : adjList.get(node)) {
-                indegree[neighbor]--;
-                if (indegree[neighbor] == 0) {
-                    q.add(neighbor);
-                }
+            for (int neig : adj.get(node)) {
+                indegree[neig]--;
+                if (indegree[neig] == 0)
+                    q.add(neig);
             }
         }
 
         return count == numCourses;
-
     }
 }
