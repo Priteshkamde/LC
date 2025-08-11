0bc43966384913b14cb097b8735cd49808a709cf
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-08-11 00:26:38 -0700

    Time: 5 ms (76.8%), Space: 46.1 MB (55.17%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,38 +1,36 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
+
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
 
-        for(int i = 0 ; i < numCourses ; i++) {
+        for (int i = 0; i < numCourses; i++)
             adj.add(new ArrayList<>());
-        }
 
-        for(int[] preq : prerequisites) {
-            adj.get(preq[1]).add(preq[0]);
-        }
-
+        // indegree
         int[] indegree = new int[numCourses];
-        for(int i = 0 ; i < numCourses ; i++) {
-            for(int neighbor : adj.get(i)){
-                indegree[neighbor]++;
-            }
+
+        for (int[] p : prerequisites) {
+            adj.get(p[1]).add(p[0]);
+            indegree[p[0]]++;
         }
 
+        // BFS
         Queue<Integer> q = new LinkedList<>();
-
-        for(int i = 0 ; i < indegree.length ; i++) {
-            if(indegree[i] == 0)
+        for (int i = 0; i < indegree.length; i++) {
+            if (indegree[i] == 0)
                 q.add(i);
         }
-
         int count = 0;
         List<Integer> result = new ArrayList<>();
-        while(!q.isEmpty()) {
+
+        while (!q.isEmpty()) {
             int node = q.poll();
             count++;
             result.add(node);
-            for(int neighbor : adj.get(node)) {
+
+            for (int neighbor : adj.get(node)) {
                 indegree[neighbor]--;
-                if(indegree[neighbor] ==0 ) {
+                if (indegree[neighbor] == 0) {
                     q.add(neighbor);
                 }
             }
@@ -45,6 +43,7 @@ class Solution {
             }
             return res;
         }
+
         return new int[]{};
     }
 }
