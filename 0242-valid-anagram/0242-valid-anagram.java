e0d892c8178e22d09e23b93b0510f2f07d802572
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-09-10 17:36:08 -0500

    Time: 2 ms (97.77%), Space: 43.4 MB (63.7%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,23 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-
+        
         if(s.length() != t.length())
             return false;
-        
-        int[] chars = new int[26];
 
-        for(char c : s.toCharArray()) {
-            chars[c-'a']++;
-        } 
+        int[] ch = new int[26];
 
-        for(char c : t.toCharArray()) {
-            chars[c-'a']--;
+        for(char c : s.toCharArray())
+            ch[c - 'a']++;
 
-            if(chars[c-'a'] < 0) {
+        for(char c : t.toCharArray())
+            if(--ch[c-'a'] < 0)
                 return false;
-            }
-        } 
-
+            
         return true;
     }
 }
