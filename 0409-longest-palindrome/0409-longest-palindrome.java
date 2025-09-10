938a6b1aa34f2a954e441048036e4260eabf987b
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-09-10 17:47:12 -0500

    Time: 7 ms (53.5%), Space: 42.1 MB (40.57%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,26 +1,25 @@
 class Solution {
     public int longestPalindrome(String s) {
-        
+
         Map<Character, Integer> map = new HashMap<>();
 
-        for(char c : s.toCharArray()) {
+        for (char c : s.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
         int total = 0;
         boolean hasOddElement = false;
-        for(char c : map.keySet()) {
+        for (char c : map.keySet()) {
             int freq = map.get(c);
 
-            if(freq % 2 == 0){
+            if (freq % 2 == 0) {
                 total += freq;
             } else {
-                total += (freq-1);
+                total += (freq - 1);
                 hasOddElement = true;
             }
         }
 
         return (hasOddElement) ? total + 1 : total;
-
     }
 }
