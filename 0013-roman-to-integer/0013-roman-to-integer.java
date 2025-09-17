0d2dcb4f16d752fcd7be141c3d492c8d20426b46
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-09-17 02:11:09 -0500

    Time: 5 ms (57.38%), Space: 44.9 MB (52.1%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,9 +1,7 @@
 class Solution {
     public int romanToInt(String s) {
-        int sum  = 0;
         
-        HashMap<Character, Integer> hm = new HashMap<>();
-        
+        Map<Character, Integer> hm = new HashMap<>();
         hm.put('I', 1);
         hm.put('V',5);
         hm.put('X',10);
@@ -11,31 +9,20 @@ class Solution {
         hm.put('C',100);
         hm.put('D',500);
         hm.put('M',1000);
-        
-        // IV
-// 1 + 5 - 2 
-        
-        for(int i = 0; i< s.length() ; i++) {
-            if(s.charAt(i)=='V'||s.charAt(i)=='X'){
-                if(i>0&&s.charAt(i-1)=='I'){
-                    sum = sum - 2;
-                }
-            }
-            
-            if(s.charAt(i)=='L'||s.charAt(i)=='C'){
-                if(i>0&&s.charAt(i-1)=='X'){
-                    sum = sum - 20;
-                }
-            }
-            
-            if(s.charAt(i)=='D'||s.charAt(i)=='M'){
-                if(i>0&&s.charAt(i-1)=='C'){
-                    sum = sum - 200;
-                }
-            }
-            sum += hm.get(s.charAt(i));
-        }
 
+        int N = s.length()-1;
+        int sum = 0;
+
+        for(int i = 0; i < N ; i++) {
+            if(hm.get(s.charAt(i)) < hm.get(s.charAt(i+1))) {
+                sum = sum - hm.get(s.charAt(i));
+            }
+            else {
+                sum = sum + hm.get(s.charAt(i));
+            }
+        }
+        
+        sum = sum + hm.get(s.charAt(s.length()-1));
         return sum;
     }
 }
