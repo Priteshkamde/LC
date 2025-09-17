71d66c0760c3ca953c517d0280a157a79de44da0
Pk <40750377+Priteshkamde@users.noreply.github.com>
2025-09-17 02:11:15 -0500

    Time: 5 ms (57.38%), Space: 44.5 MB (97.4%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,28 +1,27 @@
 class Solution {
     public int romanToInt(String s) {
-        
-        Map<Character, Integer> hm = new HashMap<>();
-        hm.put('I', 1);
-        hm.put('V',5);
-        hm.put('X',10);
-        hm.put('L',50);
-        hm.put('C',100);
-        hm.put('D',500);
-        hm.put('M',1000);
+        Map<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
 
-        int N = s.length()-1;
-        int sum = 0;
+        int sol = 0;
+        int N = s.length() - 1;
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < N; i++) {
 
-        for(int i = 0; i < N ; i++) {
-            if(hm.get(s.charAt(i)) < hm.get(s.charAt(i+1))) {
-                sum = sum - hm.get(s.charAt(i));
-            }
-            else {
-                sum = sum + hm.get(s.charAt(i));
+            if(romanMap.get(arr[i]) < romanMap.get(arr[i+1])) {
+                sol = sol - romanMap.get(arr[i]);
+            } else {
+                sol = sol + romanMap.get(arr[i]);
             }
         }
-        
-        sum = sum + hm.get(s.charAt(s.length()-1));
-        return sum;
+
+        sol = sol + romanMap.get(arr[arr.length-1]); 
+        return sol;
     }
 }
